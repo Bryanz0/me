7151c79193220d2ed1e02ed7938d2260aee8c534
Bryan Zambrano <bryanz557@gmail.com>
2023-10-30 16:44:39 -0500

    setting up project

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import './css/App.css'
+import { ThemeContext } from './contexts/Theme.context'
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import Header from './views/partials/Header';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+
+  //Detect the browser's default mode
+  const isBrowserModeDark = () => window.matchMedia('(prefers-color-scheme: dark)').matches;
+  // Get the default theme in the localStorage
+  const getDefaultTheme = () =>{
+    const localStorageTheme = localStorage.getItem('theme');
+    const browserDefaultTheme = isBrowserModeDark() ? 'dark' : 'light'
+    return localStorageTheme || browserDefaultTheme;
+  } 
+
+  const [theme, setTheme] = useState(getDefaultTheme);
+
+  //aos animations init
+  useEffect(()=>{
+    AOS.init();
+  })
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ThemeContext.Provider value={{theme,setTheme}}>
+      <HelmetProvider>
+        <div className={'bg-body-'+theme}>
+          <Helmet>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.8.0/gsap.min.js" type="text/javascript" />
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.8.0/ScrollTrigger.min.js" type="text/javascript" />
+          </Helmet>
+          <Header />
+        </div>
+      </HelmetProvider>
+    </ThemeContext.Provider>
   )
 }
 
