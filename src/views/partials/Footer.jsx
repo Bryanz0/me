74a38221de7c88815aa152caf79adfc16b947043
Bryan Zambrano <bryanz557@gmail.com>
2024-02-19 23:29:20 -0500

    Update CV and experiencie dates, and correct some information

--- src/views/partials/Footer.jsx
+++ src/views/partials/Footer.jsx
@@ -8,7 +8,7 @@ function Footer () {
         <footer className="my_footer">
           <div className="content has-text-centered">
             <p>{t("footerSection.info1")} <br/><a href="https://react.dev/">React</a>, <a href="https://bulma.io/">Bulma </a> 
-            {t("footerSection.info2")}</p>
+            {t("footerSection.info2")} &#127775;</p>
             <p>{t("footerSection.info3")} <a href="https://github.com/Bryanz0/template-react-portfolio">repo</a> :D</p>
             <p style={{fontSize: "small"}}>&copy; <em>Bryan Zambrano</em> 2023 </p>
           </div>
